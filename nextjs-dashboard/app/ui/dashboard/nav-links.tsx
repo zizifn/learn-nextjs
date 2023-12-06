36a309aa1ccea8668180b0a69c83938a2c4106b1
zizifn <1803942+zizifn@users.noreply.github.com>
2023-12-06 19:25:28 +0800

    init project

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -38,7 +38,7 @@ export default function NavLinks() {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
-          >  >
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
